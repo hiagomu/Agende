a614143bf84f281b89a51c61458fb837bc20a6da
jarbas.282505 <jarbaspin@gmail.com>
2020-11-18 18:06:27 -0300

    Configuration of NavConfig

--- app/src/main/java/com/example/loginactivity/NavConfig.java
+++ app/src/main/java/com/example/loginactivity/NavConfig.java
@@ -1,14 +1,31 @@
 package com.example.loginactivity;
 
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.os.Bundle;
 
+import androidx.fragment.app.DialogFragment;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.example.loginactivity.ui.NavActivity;
 
 public class NavConfig extends Fragment {
+    private TextView textGeral;
+    private TextView textNotify;
+    private TextView textVisual;
+    private TextView textContato;
+    private TextView textSobre;
+    private AlertDialog alerta;
+    private Context context;
 
     public NavConfig() {
         // Required empty public constructor
@@ -23,6 +40,37 @@ public class NavConfig extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_config, container, false);
+        context = getContext();
+        View view = inflater.inflate(R.layout.fragment_nav_config, container, false);
+        textGeral = view.findViewById(R.id.configureTextGeral);
+
+        textSobre = view.findViewById(R.id.configureTextSobre);
+        textSobre.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(),"Teste",Toast.LENGTH_LONG).show();
+                alerta();
+            }
+        });
+        return view;
+    }
+    private void alerta(){
+        AlertDialog.Builder builder = new AlertDialog.Builder(context);
+        builder.setTitle(R.string.configure_nav_sobre_info_title);
+        builder.setMessage(R.string.configure_nav_sobre_info);
+        builder.setPositiveButton(R.string.configure_nav_sobre_info_y, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"positivy button", Toast.LENGTH_SHORT).show();
+            }
+        });
+        builder.setNegativeButton(R.string.configure_nav_sobre_info_n, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                Toast.makeText(getContext(),"negative button",Toast.LENGTH_SHORT).show();
+            }
+        });
+        alerta = builder.create();
+        alerta.show();
     }
 }
--- app/src/main/java/com/example/loginactivity/NavHome.java
+++ app/src/main/java/com/example/loginactivity/NavHome.java
@@ -3,12 +3,15 @@ package com.example.loginactivity;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 public class NavHome extends Fragment {
+    private Button button;
 
     public NavHome() {
         // Required empty public constructor
@@ -23,6 +26,14 @@ public class NavHome extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nav_home, container, false);
+        View view = inflater.inflate(R.layout.fragment_nav_home, container, false);
+        button = view.findViewById(R.id.button);
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Navigation.findNavController(view).navigate(R.id.action_navHome_to_navConfig);
+            }
+        });
+        return view;
     }
 }
